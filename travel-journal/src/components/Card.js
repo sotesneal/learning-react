08f5341c50011bdbb2d23ceb6eb3655907bc91ad
sotesneal <nsotes@fullscale.io>
2022-05-12 10:19:04 +0800

    change overall style using material ui

--- travel-journal/src/components/Card.js
+++ travel-journal/src/components/Card.js
@@ -9,28 +9,99 @@ import {
   Toolbar,
   Container,
   CssBaseline,
+  Tooltip,
+  Link,
+  List,
+  ListItem,
 } from "@mui/material";
 import { padding } from "@mui/system";
 import PublicIcon from "@mui/icons-material/Public";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 export default function CardComponent(props) {
   console.log(props);
   return (
-    <Card sx={{ maxWidth: 345, marginTop: 2 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={props.image}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.place}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.description}
-        </Typography>
-      </CardContent>
-    </Card>
+    <List
+      sx={{
+        width: "100%",
+        maxWidth: 900,
+        margin: "auto",
+      }}
+    >
+      <ListItem>
+        <Box
+          component="img"
+          sx={{
+            height: 233,
+            width: 350,
+          }}
+          alt="The house from the offer."
+          src={props.image}
+        ></Box>
+        <CardContent>
+          <li>
+            <Toolbar>
+              <LocationOnIcon style={{ color: "#F55A5A" }} />
+              <Typography>{props.country}</Typography>
+              <Link
+                href={props.googleMapUrl}
+                sx={{ marginLeft: 2 }}
+                variant="body2"
+              >
+                {"View on Google Maps"}
+              </Link>
+            </Toolbar>
+          </li>
+          <Typography gutterBottom variant="h4">
+            {props.place}
+          </Typography>
+          <Typography variant="h7" component="div">
+            {props.date}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </ListItem>
+    </List>
   );
+}
+{
+  /* <Card sx={{ marginTop: 2 }}>
+  <CardMedia
+    component="img"
+    alt="green iguana"
+    height="400"
+    image={props.image}
+  />
+  <CardContent>
+    <Toolbar>
+      <LocationOnIcon />
+      <Typography>{props.country}</Typography>
+      <Link href={props.googleMapUrl} sx={{ marginLeft: 2 }} variant="body2">
+        {"View on Google Maps"}
+      </Link>
+    </Toolbar>
+    <Typography gutterBottom variant="h4">
+      {props.place}
+    </Typography>
+    <Typography variant="h7" component="div">
+      {props.date}
+    </Typography>
+    <Typography variant="body2" color="text.secondary">
+      {props.description}
+    </Typography>
+  </CardContent>
+</Card>;
+ */
 }
--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -1,26 +1,29 @@
 export default [
   {
-    image: "https://semantic-ui.com/images/wireframe/image.png",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/1/1b/080103_hakkai_fuji.jpg",
     country: "JAPAN",
-    googleMapUrl: "",
+    googleMapUrl: "https://goo.gl/maps/KKy2VmD6RAcx3obBA",
     place: "Mount Fuji",
     date: "12 Jan, 2021 - 24 Jan, 2021",
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
   },
   {
-    image: "https://semantic-ui.com/images/wireframe/image.png",
+    image:
+      "https://cdn.britannica.com/97/60997-050-7F853611/Sydney-Opera-House-Harbour-Bridge.jpg",
     country: "AUSTRALIA",
-    googleMapUrl: "",
+    googleMapUrl: "https://goo.gl/maps/vJEHw1sgya21hPeP9",
     place: "Sydney Opera House",
     date: "27 May, 2021 - 8 Jun, 2021",
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
   },
   {
-    image: "https://semantic-ui.com/images/wireframe/image.png",
+    image:
+      "http://res.cloudinary.com/simpleview/image/upload/v1450117455/clients/norway/unesco-geirangerfjord-skagefla-waterfall-2-1_6cc6a64a-a204-432e-8753-01ef2080f24e.jpg",
     country: "NORWAY",
-    googleMapUrl: "",
+    googleMapUrl: "https://goo.gl/maps/MQy6NsJ3V6LjkmnJ9",
     place: "Geirangerfjord",
     date: "01 Oct, 2021 - 18 Nov, 2021",
     description:
